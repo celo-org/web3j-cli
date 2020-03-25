46dfff2b890c2444b57b9009c9fcaf5050deab8e
Amy Slawson <amyslawson@MacBook-Pro.local>
2020-03-25 09:25:45 -0700

    add support for transfer stabletoken

--- src/main/java/org/web3j/console/Runner.java
+++ src/main/java/org/web3j/console/Runner.java
@@ -12,6 +12,8 @@
  */
 package org.web3j.console;
 
+import java.math.BigInteger;
+
 import org.web3j.codegen.Console;
 import org.web3j.codegen.SolidityFunctionWrapperGenerator;
 import org.web3j.codegen.TruffleJsonFunctionWrapperGenerator;
@@ -21,12 +23,20 @@ import org.web3j.console.project.ProjectCreator;
 import org.web3j.console.project.ProjectImporter;
 import org.web3j.console.project.UnitTestCreator;
 import org.web3j.console.update.Updater;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.StaticGasProvider;
 import org.web3j.utils.Version;
 
 import static org.web3j.codegen.SolidityFunctionWrapperGenerator.COMMAND_SOLIDITY;
 import static org.web3j.console.project.ProjectCreator.COMMAND_NEW;
 import static org.web3j.console.project.ProjectImporter.COMMAND_IMPORT;
 import static org.web3j.console.project.UnitTestCreator.COMMAND_GENERATE_TESTS;
+import static org.web3j.crypto.Hash.sha256;
 import static org.web3j.utils.Collection.tail;
 
 /** Main entry point for running command line utilities. */
@@ -58,6 +68,7 @@ public class Runner {
         if (args.length < 1) {
             Console.exitError(USAGE);
         } else {
+            Web3j web3j;
             switch (args[0]) {
                 case "wallet":
                     WalletRunner.run(tail(args));
@@ -91,6 +102,75 @@ public class Runner {
                 case COMMAND_GENERATE_TESTS:
                     UnitTestCreator.main(tail(args));
                     break;
+                case "block":
+                    web3j =
+                            Web3j.build(
+                                    new HttpService("https://alfajores-forno.celo-testnet.org"));
+                    BigInteger blockNumber = new BigInteger("761206");
+                    EthBlock.Block block =
+                            web3j.ethGetBlockByNumber(
+                                            DefaultBlockParameter.valueOf(blockNumber), true)
+                                    .send()
+                                    .getResult();
+                    EthBlock.TransactionResult<EthBlock.TransactionObject> result =
+                            block.getTransactions().get(0);
+                    System.out.println(
+                            String.format(
+                                    "Fee Currency: %s \nGateWayRecipient: %s \nGateWayFee %s \n",
+                                    result.get().getFeeCurrency(),
+                                    result.get().getGatewayFeeRecipient(),
+                                    result.get().getGatewayFee()));
+                    Console.exitSuccess("Fee Currency: \n" + result.get().getFeeCurrency());
+                case "transfer":
+                    // TODO [amyslawson] pull logic into seprate java file and add test
+                    String to = args[1]; // eg. 0x7963325c601bec69b9b43dd3ebd07c4911106c93
+                    BigInteger value = new BigInteger(args[2]);
+                    web3j =
+                            Web3j.build(
+                                    new HttpService("https://alfajores-forno.celo-testnet.org"));
+                    // TODO [amyslawson] sign with sender
+                    ECKeyPair ecKeyPair = ECKeyPair.create(sha256("test_sk".getBytes()));
+                    Credentials credentials = Credentials.create(ecKeyPair);
+                    // TODO [amyslawson] change address to be downloaded based on env (pull in
+                    // protocol
+                    // download_artifacts.sh) and prepopulated in Contracts.java deployedAddresses
+                    // map
+                    StableToken stableToken =
+                            StableToken.load(
+                                    // TODO [amyslawson] move contract java wrapper generation into
+                                    // web3j
+                                    "0xE94ad4f222e3b4E6a1fdf209801E5f76Ff30d50F",
+                                    web3j,
+                                    credentials,
+                                    new StaticGasProvider(
+                                            // TODO [amyslawson] get gasPriceMinimum from contract
+                                            BigInteger.valueOf(2000000000),
+                                            BigInteger.valueOf(20000000)));
+
+                    String name = stableToken.name().send();
+                    // TODO [amyslawson] remove print statements and convert to tests
+                    System.out.println(
+                            "BEFORE SENDER: "
+                                    + stableToken
+                                            .balanceOf("0x0ed3510bf7adcce8c48eddf05df353407c578da5")
+                                            .send());
+                    System.out.println(
+                            "BEFORE RECEVER: "
+                                    + stableToken
+                                            .balanceOf("0x7963325c601bec69b9b43dd3ebd07c4911106c93")
+                                            .send());
+                    stableToken
+                            .transfer(
+                                    "0x7963325c601bec69b9b43dd3ebd07c4911106c93",
+                                    BigInteger.valueOf(2000))
+                            .send();
+                    System.out.println(
+                            "AFTER RECEVER: "
+                                    + stableToken
+                                            .balanceOf("0x7963325c601bec69b9b43dd3ebd07c4911106c93")
+                                            .send());
+                    Console.exitSuccess(
+                            String.format("Exchanged: %s %s from to %s", value, name, to));
                 default:
                     Console.exitError(USAGE);
             }
